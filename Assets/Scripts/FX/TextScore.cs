3e5e1d76954c90a5e90d486fb4e93b1915d97a50
LHouyez <l.houyez@rubika-edu.com>
2020-01-14 20:21:16 +0100

    TExtScore + modif Broyeur , modif Decharge

---
+++ Assets/Scripts/FX/TextScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TextScore : PoolableObjects
+{
+    public Transform myTransform;
+    public TextMeshPro textScore;
+    public Animator anim;
+
+    private void Awake()
+    {
+        if (myTransform == null)
+        {
+            myTransform = transform;
+        }
+    }
+
+    public void PlayAnim()
+    {
+        anim.Play("addScore");
+    }
+
+
+
+
+}
