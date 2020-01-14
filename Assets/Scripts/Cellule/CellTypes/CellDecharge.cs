29ef9930b24d2f760ccdfe1bc584cae880b0e916
LHouyez <l.houyez@rubika-edu.com>
2020-01-14 20:45:04 +0100

    Changement decharge

--- Assets/Scripts/Cellule/CellTypes/CellDecharge.cs
+++ Assets/Scripts/Cellule/CellTypes/CellDecharge.cs
@@ -8,21 +8,21 @@ public class CellDecharge : CellMain
     {
         if (blobNumber > 0)
         {
-            BlobNumberVariation(-1 , BlobCheck());
+            BlobNumberVariation(-1, BlobCheck());
             //CellManager.Instance.EnergyVariation(currentEnergyPerClick);
             RessourceTracker.instance.EnergyVariation(specifiqueStats);
+            TextScore newTextescore = ObjectPooler.poolingSystem.GetPooledObject<TextScore>() as TextScore;
+            newTextescore.Outpool();
+
+
+            Debug.Log(myTransform.position);
+            newTextescore.myTransform.position = myTransform.position + new Vector3(Random.Range(-0.5f, 0.5f), 2, 0);
+            newTextescore.textScore.text = ("+" + specifiqueStats.ToString());
+            newTextescore.PlayAnim();
         }
         // à quoi servent les 2 dernier parametre
         //AnimBroyage
         anim.Play("PlayerInteraction", 0, 0f);
-        TextScore newTextescore = ObjectPooler.poolingSystem.GetPooledObject<TextScore>() as TextScore;
-        newTextescore.Outpool();
-
-
-        Debug.Log(myTransform.position);
-        newTextescore.myTransform.position = myTransform.position + new Vector3 (Random.Range(-0.5f,0.5f) , 2,0);
-        newTextescore.textScore.text = ("+" + specifiqueStats.ToString());
-        newTextescore.PlayAnim();
 
     }
 
