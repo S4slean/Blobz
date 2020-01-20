d47bfee1c043ff8592c579ed3cc0ccd5c5351d35
LHouyez <l.houyez@rubika-edu.com>
2020-01-20 20:06:35 +0100

    Productrice regen + bar fix

--- Assets/Scripts/Cellule/CellTypes/Specificite/ProgressBar.cs
+++ Assets/Scripts/Cellule/CellTypes/Specificite/ProgressBar.cs
@@ -8,10 +8,18 @@ public class ProgressBar : MonoBehaviour
     public Image filledImage;
     public Animator anim;
 
+    public GameObject graph;
+
     public void UpdateBar(float ratio)
     {
         filledImage.fillAmount = ratio;
         anim.speed = 1 / (TickManager.instance.tickDuration - TickManager.instance.tickDuration / 1.8f);
         anim.Play("AmountModification");
     }
+
+    public void ToggleRenderer(bool toggle)
+    {
+        graph.SetActive(toggle);
+    }
+
 }
