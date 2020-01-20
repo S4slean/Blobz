adc1ae98ef89e3eace521c57e6ddae04a4c8997c
Slean <s.dubuc@rubika-edu.com>
2020-01-20 19:22:23 +0100

    refix

--- Assets/Scripts/Quests/QuestManager.cs
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -244,7 +244,7 @@ public class QuestManager : MonoBehaviour
 
                 case QuestEvent.QuestEventType.PopUp:
 
-                    if (currentQuestID == 0)
+                    if (currentMsgID == 0)
                     {
                         QuestEventManager.instance.popUpIsSkippable = false;
                         StartCoroutine(WaitBeforeSkip());
