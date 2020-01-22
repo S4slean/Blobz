de3a476c236119007aa564779affaa1a9c54c1ef
LHouyez <l.houyez@rubika-edu.com>
2020-01-22 19:22:28 +0100

    Distribution View lien , prod

---
+++ Assets/Scripts/Link/LinkTransmissionNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkTransmissionNumber : MonoBehaviour
+{
+    private Transform myTransform;
+
+    private void Awake()
+    {
+        if (myTransform == null)
+        {
+            myTransform = transform;
+        }
+        
+    }
+
+    public void UpdatePosAndScale(Vector3 pos , float angle)
+    {
+        myTransform.position = pos + new Vector3(0, 0.2f, 0);
+        myTransform.rotation = Quaternion.Euler(90, angle%90 - 90, 0);
+    }
+
+}
