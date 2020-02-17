014931dc0b185f39b342c70b02b1470ae265c8dd
LHouyezGlobalsoul <houyez.louis.lille@gmail.com>
2020-02-17 13:10:47 +0100

    meta

---
+++ Assets/Scripts/Utilities/VisibleCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibleCheck : MonoBehaviour
+{
+
+    private bool isVisible;
+
+    private void OnBecameInvisible()
+    {
+        isVisible = false;
+
+    }
+    private void OnBecameVisible()
+    {
+        isVisible = true;
+    }
+
+    private void Update()
+    {
+        if (isVisible)
+        {
+            Debug.Log("Is Visible ,object :" , gameObject);
+        }
+        else
+        {
+            Debug.Log("Invisible ,object :" , gameObject);
+
+        }
+    }
+}
